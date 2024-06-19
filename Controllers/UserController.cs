cbaa03c4af2d832afbefe489cf8128c6c3f9cd85
github_cloudlabsuser_2045@cloudlabsaiuser.com <github_cloudlabsuser_2045@cloudlabsaiuser.com>
2024-06-19 05:16:34 +0000

    Enhanced testing and added search feature
    - Integrated NUnit and xUnit frameworks for unit testing in `CRUD application 2.csproj`.
    - Added NuGet packages for testing, mocking, and async enhancements, including `Castle.Core`, `Moq`, `MSTest.TestFramework`, and others.
    - Introduced x64 configuration for both Debug and Release modes in solution and project files.
    - Added `azuredeploy.json` for Azure deployment setup, preparing for cloud deployment.
    - Implemented search functionality in `UserController.cs` and added `Search.cshtml` view.
    - Updated `Web.config` for `WebGrease` version alignment.
    - Updated `Index.cshtml` to link to the new search page and added new views to support search functionality.
    - Updated solution file for Visual Studio 17 and added `UnitTestProject1` for unit tests.
    - Updated `packages.config` to reflect new NuGet package additions.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -11,18 +11,31 @@ namespace CRUD_application_2.Controllers
         public ActionResult Index()
         {
             // Implement the Index method here
+            userlist.GetEnumerator();
+            return View(userlist);
         }
  
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
-            // Implement the details method here
+            // return the user with the specified ID
+            // If no user is found with the provided ID, return HttpNotFoundResult
+            // If successful, return the Details view
+            userlist.GetEnumerator();
+            var user = userlist.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
  
         // GET: User/Create
         public ActionResult Create()
         {
             //Implement the Create method here
+            // add the user to the userlist
+            return View("Create");
         }
  
         // POST: User/Create
@@ -30,6 +43,12 @@ namespace CRUD_application_2.Controllers
         public ActionResult Create(User user)
         {
             // Implement the Create method (POST) here
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            userlist.Add(user);
+            return RedirectToAction("Index");
         }
  
         // GET: User/Edit/5
@@ -37,6 +56,15 @@ namespace CRUD_application_2.Controllers
         {
             // This method is responsible for displaying the view to edit an existing user with the specified ID.
             // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            // If no user is found with the provided ID, it returns a HttpNotFoundResult.
+            // If successful, it returns the Edit view.
+            userlist.GetEnumerator();
+            var user = userlist.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View("Edit", user);
         }
  
         // POST: User/Edit/5
@@ -48,12 +76,30 @@ namespace CRUD_application_2.Controllers
             // If successful, it redirects to the Index action to display the updated list of users.
             // If no user is found with the provided ID, it returns a HttpNotFoundResult.
             // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            userlist.GetEnumerator();
+            var userToUpdate = userlist.FirstOrDefault(x => x.Id == id);
+            if (userToUpdate == null)
+            {
+                return HttpNotFound();
+            }
+            userToUpdate.Name = user.Name;
+            userToUpdate.Email = user.Email;
+            return RedirectToAction("Index");
         }
  
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
-            // Implement the Delete method here
+            // Delete the user with the specified ID
+            // If no user is found with the provided ID, return HttpNotFoundResult
+            // If successful, return the Index view
+            var userToDelete = userlist.FirstOrDefault(x => x.Id == id);
+            if (userToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            return View("Delete", userToDelete);
+
         }
  
         // POST: User/Delete/5
@@ -61,6 +107,31 @@ namespace CRUD_application_2.Controllers
         public ActionResult Delete(int id, FormCollection collection)
         {
             // Implement the Delete method (POST) here
+            var userToDelete = userlist.FirstOrDefault(x => x.Id == id);
+            if (userToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            userlist.Remove(userToDelete);
+            return RedirectToAction("Index");
+        }
+
+
+        // GET: search
+        // This method is responsible for displaying the search view and can have query parameters to search for users based on specific criteria.
+        public ActionResult Search(string name)
+        {
+            // Implement the Search method here
+            // It retrieves the user from the userlist based on the provided name and passes it to the Search view.
+            // If no user is found with the provided name, it returns a HttpNotFoundResult.
+            // If successful, it returns the Search view.
+            userlist.GetEnumerator();
+            var user = userlist.FirstOrDefault(x => x.Name == name);
+            if (user == null)
+            {
+                return View("Search");
+            }
+            return View("Search", user);
         }
     }
 }
